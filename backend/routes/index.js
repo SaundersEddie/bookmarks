e000cf3e3c663cd47cf2c8896ec939b29ecf5ef0
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-21 11:58:32 -0500

    Added in some route coding to test

---
+++ backend/routes/index.js
@@ -0,0 +1,22 @@
+// Update this file with routes as they're added
+
+const path = require('path');
+const router = require('express').Router();
+// const authRoutes = require('./auth');
+// const apiRoutes = require('./api');
+
+router.route('/api') {
+
+}
+// Auth Routes
+// router.use('/auth', authRoutes)
+
+// API Routes
+// router.use('/api', apiRoutes)
+
+// If no routes are hit, send the React app
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+module.exports = router

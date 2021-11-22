8f49f1377dbb3b40cb33087a15ad8fe712a89878
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-21 19:29:23 -0500

    Added in userController code

---
+++ backend/controllers/index.js
@@ -0,0 +1,1 @@
+// Group contollers go here
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,59 @@
 const db = require('../models');
 const bcrypt = require('bcryptjs')
 
+// Defining methods for the userController
 module.exports = {
-
-    // User Controller code goes in here
-
-}
+    getUser: (req, res, next) => {
+        if (req.user) {
+            return res.json({ user: req.user });
+        } else {
+            return res.json({ user: null });
+        }
+    },
+    register: (req, res) => {
+        console.log(req.body)
+        const { firstName, lastName, userName, password, userEmail, isAdmin } = req.body;
+        db.User.findOne({ userName: userName }, (err, userMatch) => {
+            if (userMatch) {
+                return res.json({
+                    error: `Sorry, already a user with the username: ${userName}`
+                });
+            }
+            const newUser = new db.User({
+                firstName: firstName,
+                lastName: lastName,
+                userName: userName,
+                password: password,
+                userEmail: userEmail,
+                isAdmin: isAdmin,
+            });
+            newUser.save((err, savedUser) => {
+                if (err) return res.json(err);
+                return res.json(savedUser);
+            });
+        });
+    },
+    logout: (req, res) => {
+        if (req.user) {
+            req.session.destroy();
+            res.clearCookie('connect.sid'); // clean up!
+            return res.json({ msg: 'logging you out' });
+        } else {
+            return res.json({ msg: 'no user to log out!' });
+        }
+    },
+    auth: function (req, res, next) {
+        console.log("In auth:", req.body);
+        next();
+    },
+    authenticate: (req, res) => {
+        console.log('In Authenticate:', req.body);
+        const user = JSON.parse(JSON.stringify(req.user)); // hack
+        const cleanUser = Object.assign({}, user);
+        if (cleanUser) {
+            // console.log(`Deleting ${cleanUser.password}`);
+            delete cleanUser.password;
+        }
+        res.json({ user: cleanUser });
+    }
+};

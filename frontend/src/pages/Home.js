27ace7e052ce4dd1cad8e700fbd95a3f9b6b664b
Eddie Saunders <saunders.eddie@outlook.com>
2021-12-05 16:35:04 -0500

    cleaned up home page and correct reposiory link

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,13 +4,11 @@ import React from 'react';
 export default function Home() {
     return (
         <>
-            <h1>Our Home Page.</h1>
+            <h1>Bookmarks</h1>
             <hr></hr>
-            <p>This is a work in progress page, so please excuse the mess while items get cleaned up and shuffled around.</p>
-            <p>The idea behind this project was to do a fairly simple react app, that implemented API coding, react, and react-bootstrap. </p>
-            <p>The code itself is not some complex code and visually pleasing masterpiece. Many improvements can still, and will be made over time.</p>
+            <p>Some waffle about the app, when really the Login page or option to register should be here.</p>
             <p>The source code can be located here:</p>
-            <p><a href="https://github.com/EXSsaunders/washingtondc" target="_blank" rel="noopener noreferrer">Source Code</a></p>
+            <p><a href="https://github.com/SaundersEddie/bookmarks" target="_blank" rel="noopener noreferrer">Source Code</a></p>
             <p>Eddie</p>
         </>
     )
